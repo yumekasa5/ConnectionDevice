3c77e868de1c10e948bc986ed89c3410fd43f5da
kaigta22 <kaitomx@gmail.com>
2022-07-01 00:49:27 +0900

    add test method

--- tests/test_correct_temp.py
+++ tests/test_correct_temp.py
@@ -1,5 +1,18 @@
 """温度補正における数値計算のテスト"""
 from ..connectiondevice.libs.correct_temp import *
 
-def test_gaussian_filter(array):
-    pass
+def test_gaussian_filter():
+    result = gaussian_filter([])
+    assert result
+
+def test_calc_offset_by_distance():
+    result = calc_offset_by_distance(30, 5, 5)
+    assert result == 155
+
+def test_get_facegrids_degC():
+    result = get_facegrids_degC([])
+    assert result == None
+
+def test_estimate_body_temp_degC():
+    result = estimate_body_temp_degC(30, [], 10)
+    assert result == 36.5
