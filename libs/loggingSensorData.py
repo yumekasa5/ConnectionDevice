9f8039a3c0a7699163a582cd365153d098cfac72
yumekasa5 <kaitomx@gmail.com>
2022-07-17 06:03:29 +0900

    wip: 更新中

--- libs/loggingSensorData.py
+++ libs/loggingSensorData.py
@@ -3,3 +3,6 @@ import numpy as np
 import pandas as pd
 
 #source test
+df = pd.DataFrame(np.array([[1, 2, 3], [5, 6, 7]]), 
+                  columns=['facegrid temp[degC]', 'distance[cm]', 'body temp[degC]'])
+print(df)
