f11315973592525393512357d8b9f907b0c9b498
yumekasa5 <kaitomx@gmail.com>
2022-07-17 15:10:02 +0900

    wip: 更新途中

--- libs/loggingSensorData.py
+++ libs/loggingSensorData.py
@@ -1,8 +1,13 @@
 """センサからのデータを保存するクラスや関数"""
+from datetime import datetime, date
 import numpy as np
 import pandas as pd
 
-#source test
-df = pd.DataFrame(np.array([[1, 2, 3], [5, 6, 7]]), 
-                  columns=['facegrid temp[degC]', 'distance[cm]', 'body temp[degC]'])
+#日付、時刻、顔グリッド温度、測定距離、推定体温
+date_now = date.today()
+time_now = datetime.now().time()
+data_array = np.array([[date_now, time_now,  35.9, 30.5, 36.4],
+                       [date_now, time_now, 36.3, 25.5, 36.6]])
+df = pd.DataFrame(data_array, 
+                  columns=['date', 'time',  'facegrid temp[degC]', 'distance[cm]', 'body temp[degC]'])
 print(df)
