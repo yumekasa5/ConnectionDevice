25d790d60419175ef3c557d4fd3eac1b702eca1e
yumekasa5 <kaitomx@gmail.com>
2022-07-17 16:31:31 +0900

    refactor: 時刻を分までに限定

--- libs/loggingSensorData.py
+++ libs/loggingSensorData.py
@@ -6,11 +6,11 @@ import pandas as pd
 #日付、時刻、顔グリッド温度、推定環境温度, 測定距離、推定体温
 date_now = date.today()
 time_now = datetime.now().time()
-test_array = np.array([[date_now, time_now,  35.9, 25.0, 30.5, 36.4],
-                       [date_now, time_now, 36.3, 25.0, 25.5, 36.6]])
+test_array = np.array([[date_now, time_now.strftime('%H:%M'),  35.9, 25.0, 30.5, 36.4],
+                       [date_now, time_now.strftime('%H:%M'), 36.3, 25.0, 25.5, 36.6]])
 test_df = pd.DataFrame(test_array, 
                   columns=['日付', '時刻',  '顔グリッド温度[degC]', 
                            '推定環境温度[degC]','測定距離[cm]', '推定体温[degC]'])
-print(test_df)
+# print(test_df)
 output_path = '../result/output.csv'
 test_df.to_csv(output_path, index = False)
