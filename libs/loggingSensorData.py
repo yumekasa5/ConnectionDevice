369b39f659cbaace67f9e6da8880d3c50d85a70e
yumekasa5 <kaitomx@gmail.com>
2022-07-17 16:09:17 +0900

    feature: CSV形式で出力

--- libs/loggingSensorData.py
+++ libs/loggingSensorData.py
@@ -9,6 +9,8 @@ time_now = datetime.now().time()
 test_array = np.array([[date_now, time_now,  35.9, 25.0, 30.5, 36.4],
                        [date_now, time_now, 36.3, 25.0, 25.5, 36.6]])
 test_df = pd.DataFrame(test_array, 
-                  columns=['date', 'time',  'facegrid temp[degC]', 
-                           'environment temp[degC]','distance[cm]', 'body temp[degC]'])
+                  columns=['日付', '時刻',  '顔グリッド温度[degC]', 
+                           '推定環境温度[degC]','測定距離[cm]', '推定体温[degC]'])
 print(test_df)
+output_path = '../result/output.csv'
+test_df.to_csv(output_path, index = False)
