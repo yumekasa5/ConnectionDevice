54f01e4fabf5d721be1d79056cf2c565f07f7deb
kaigta22 <kaitomx@gmail.com>
2022-07-20 22:36:36 +0900

    feat: 温度補正に関する各種関数追加

--- connectiondevice/libs/correct_temp.py
+++ connectiondevice/libs/correct_temp.py
@@ -23,26 +23,65 @@
 # SOFTWARE.
 
 """温度データの補正"""
+from curses import raw
 import numpy as np
+from typing import List
 
-def gaussian_filter(array):
+def gaussian_filter(src: np.ndarray) -> np.ndarray:
     """ガウシアンフィルタ"""
-    return array
+    kernel = np.array([[1/16, 2/16, 1/16],         #カーネル(size:3x3)
+                       [2/16, 4/16, 2/16],
+                       [1/16, 2/16, 1/16]])
+    m, n = kernel.shape
 
-def calc_offset_by_distance(distance, slope, intercept):
+    #畳み込みしない領域の幅
+    d = int((m-1)/2)
+    h, w = src.shape[0], src.shape[1]
+    dst = src.copy()
+
+    #空間フィルタリング
+    for y in range(d, h - d):
+        for x in range(d, w -d):
+            dst[y][x] = np.sum(src[y-d:y+d+1, x-d:x+d+1] * kernel)
+    
+    return dst
+
+def calc_offset_by_distance(distance: float, slope: float, intercept: float) -> float:
     """距離によるオフセットの算出"""
     offset = slope * distance + intercept
     return offset
 
-def get_facegrids_degC(facegrid_array):
+def get_facegrids_degC(facegrid_array: List[float]) -> float:
     """顔と推定されるグリッドの平均値を算出"""
     ave_grid_degC = (facegrid_array[4, 5] + facegrid_array[4, 6] 
                    + facegrid_array[5, 5] + facegrid_array[5, 6]) / 4
     return ave_grid_degC
 
-def estimate_body_temp_degC(distance_cm, raw_grids, temp_offset):
+def estimate_body_temp_degC(raw_grids: List[float], temp_offset: float) -> float:
     """体温推定"""
     grids = gaussian_filter(raw_grids)
     facegrids = get_facegrids_degC(grids)
     ret = facegrids + temp_offset
     return ret
+
+def estimate_env_temp_degC(raw_grids: List[List[float]], pos_exclude: List[List[int]]) -> float:
+    """環境温度の推定"""
+    env_temp_degC = 0.0
+    h, w = np.shape(raw_grids)[0], np.shape(raw_grids)[1]
+
+    #環境温度の推定に利用するグリッド数の算出
+    if (pos_exclude == [[]]):
+        cnt = h * w
+    else:
+        cnt = h * w - len(pos_exclude)
+
+    #人体以外のグリッドの平均値算出
+    for y in range(0, h):
+        for x in range(0, w):
+            if ([y, x] in pos_exclude):
+                env_temp_degC += 0
+            else:
+                env_temp_degC += raw_grids[y][x]
+    env_temp_degC /= cnt
+    return env_temp_degC
+        
